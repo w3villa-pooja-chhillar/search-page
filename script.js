8b12de476100d10f90e87c2f9d4b24cf3a1139e2
Pooja Chhillar <pooja.chhillar@w3villa.com>
2023-04-03 12:16:49 +0530

    second commit

--- script.js
+++ script.js
@@ -2,8 +2,20 @@
 //    alert("grid-view");
 // }
 
-function addpart(){
+function addpart_price(){
    document.getElementsByClassName('add-part')[0].style.display="block";
+}
+function addpart_availbility(){
+   document.getElementsByClassName('add-part-avail')[0].style.display="block";
+}
+function addpart_subcategory(){
+   document.getElementsByClassName('add-part-subcat')[0].style.display="block";
+}
+function addpart_color(){
+   document.getElementsByClassName('add-part-color')[0].style.display="block";
+}
+function addpart_brand(){
+   document.getElementsByClassName('add-part-brand')[0].style.display="block";
 }
 function listview(){
    document.getElementsByClassName('list-items')[0].style.display="block";
